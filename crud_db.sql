5547c570a5f4046cdb3c933ecb1bebe0c7e4c063
Lizden <lruanom5@miumg.edu.gt>
2025-07-01 01:47:44 -0600

    modificaciones db

--- crud_db.sql
+++ crud_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 27, 2025 at 10:43 AM
+-- Generation Time: Jul 01, 2025 at 09:46 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -41,7 +41,7 @@ CREATE TABLE `colaborador` (
 --
 
 INSERT INTO `colaborador` (`id`, `empresaId`, `nombreCompleto`, `edad`, `telefono`, `correo`) VALUES
-(1, 1, 'Juan Robles', 34, '9001-0378', 'juan.perez@empresa.com');
+(4, 4, 'Jose Luis Morales', 24, '34567654', 'joluis@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -60,7 +60,9 @@ CREATE TABLE `departamento` (
 --
 
 INSERT INTO `departamento` (`id`, `nombre`, `paisId`) VALUES
-(1, 'Alta Verapaz', 1);
+(1, 'Alta Verapaz', 1),
+(2, 'Managua', 2),
+(3, 'Boaco', 2);
 
 -- --------------------------------------------------------
 
@@ -77,16 +79,17 @@ CREATE TABLE `empresa` (
   `razonSocial` varchar(191) NOT NULL,
   `nombreComercial` varchar(191) DEFAULT NULL,
   `telefono` varchar(191) DEFAULT NULL,
-  `correo` varchar(191) DEFAULT NULL
+  `correo` varchar(191) DEFAULT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `empresa`
 --
 
-INSERT INTO `empresa` (`id`, `paisId`, `departamentoId`, `municipioId`, `nit`, `razonSocial`, `nombreComercial`, `telefono`, `correo`) VALUES
-(1, 1, 1, 1, '12660091', 'Techno S.A.', 'Techno Market', '2776-9880', 'info@technomarket.com'),
-(2, 1, 1, 1, '93802309', 'Intelaf S.A.', 'Chepe Pollo', '0976-6789', 'chepe@pollo.com');
+INSERT INTO `empresa` (`id`, `paisId`, `departamentoId`, `municipioId`, `nit`, `razonSocial`, `nombreComercial`, `telefono`, `correo`, `activo`) VALUES
+(3, 2, 3, 4, '345324634', 'Chepe Pollo', 'Chepe Pollo', '45234243', 'chepepollo@gmail.com', 0),
+(4, 1, 1, 3, '324252345', 'ChinoMoto', 'Chino Moto', '34544434', 'chinomoto@gmail.com', 1);
 
 -- --------------------------------------------------------
 
@@ -105,7 +108,10 @@ CREATE TABLE `municipio` (
 --
 
 INSERT INTO `municipio` (`id`, `nombre`, `departamentoId`) VALUES
-(1, 'Cobán', 1);
+(1, 'Cobán', 1),
+(2, 'Tipitapa', 2),
+(3, 'Carchá', 1),
+(4, 'Santa Lucía', 3);
 
 -- --------------------------------------------------------
 
@@ -148,6 +154,7 @@ CREATE TABLE `_prisma_migrations` (
 --
 
 INSERT INTO `_prisma_migrations` (`id`, `checksum`, `finished_at`, `migration_name`, `logs`, `rolled_back_at`, `started_at`, `applied_steps_count`) VALUES
+('cbca3a67-9274-4f35-8966-bfd16060d5b1', '920275adce3ab5319bbc1bed64539cf86b196d6cad65c97985d1764197504d53', '2025-07-01 06:37:50.689', '20250701063750_add_activo_to_empresa', NULL, NULL, '2025-07-01 06:37:50.680', 1),
 ('dc446f1b-c7e5-4ad3-a8ba-dd9d0ae400d5', '3fe04535f96bfc6434866bfc61cc156d9ce2d74929cc3a18cad8ad2c25ae153f', '2025-06-27 04:19:58.909', '20250627041958_initial_migration', NULL, NULL, '2025-06-27 04:19:58.640', 1);
 
 --
@@ -204,25 +211,25 @@ ALTER TABLE `_prisma_migrations`
 -- AUTO_INCREMENT for table `colaborador`
 --
 ALTER TABLE `colaborador`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `departamento`
 --
 ALTER TABLE `departamento`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `empresa`
 --
 ALTER TABLE `empresa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `municipio`
 --
 ALTER TABLE `municipio`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `pais`
